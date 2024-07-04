745930f50d0dd9973ee523c68d9b84a99b9be8f3
Feudjo <tsephane@yahoo.fr>
2024-07-04 23:19:22 +0200

    feat: process test data

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -2,8 +2,9 @@
 
 from sklearn.model_selection import train_test_split
 import pandas as pd
-
-# Add the necessary imports for the starter code.
+from ml.data import process_data
+from sklearn.ensemble import RandomForestClassifier
+import pickle
 
 # Add code to load in the data.
 data = pd.read_csv("starter/data/census.csv")
@@ -25,5 +26,13 @@ X_train, y_train, encoder, lb = process_data(
 )
 
 # Proces the test data with the process_data function.
-
+X_test, y_test, _, _ = process_data(
+    test, categorical_features=cat_features, training=False, encoder=encoder, lb=lb)
 # Train and save a model.
+
+rf = RandomForestClassifier()
+rf.fit(X_train,  y_train)
+
+
+with open("starter/model/rf.pkl", "wb") as file:
+    pickle.dump(rf, file)
