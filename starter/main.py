ba3d81ae579fba269bd495e8b779d17664e312f8
Feudjo <tsephane@yahoo.fr>
2024-07-09 21:48:43 +0200

    feat: add api root page

--- starter/main.py
+++ starter/main.py
@@ -1,1 +1,40 @@
-# Put the code for your API here.
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from pydantic import BaseModel
+
+class CensusDataRecord(BaseModel):
+
+    age:int
+    workclass: str
+    fnlwgt: int
+    education: str
+    education_num: int
+    marital_status: str
+    occupation: str
+    relationship: str
+    race: str
+    sex: str
+
+
+
+app  = FastAPI()
+
+@app.get("/")
+async def read_root():
+    html_content = """
+                <html>
+                    <head>
+                        <title>Welcome to Income Predictor API</title>
+                    </head>
+                    <body>
+                        <h1>Welcome to the Income Predictor API!</h1>
+                        <p>Submit census data to find out if income exceeds $50K/year.</p>
+                    </body>
+                </html>
+                 """
+    return HTMLResponse(content=html_content)
+
+@app.post("/record")
+async def predict_income(item: CensusDataRecord):
+    pass
